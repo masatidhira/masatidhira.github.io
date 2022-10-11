ccd8831cb7c0663b04646d23ed1376123382c64b
masatidhira <masatidhira@gmail.com>
2022-10-11 21:12:23 +0700

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./App.css";
+import Navigation from "./components/Navigation";
 
 
 const App = () => {
   return (
-    <div className="App">
-      PORTFOLIO
+    <div>
+      <Navigation />      
     </div>
   );
 }
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Navigation = () => {
+    return (
+        <div>
+            <h1>NAV</h1>
+        </div>
+    );
+}
+
+export default Navigation;

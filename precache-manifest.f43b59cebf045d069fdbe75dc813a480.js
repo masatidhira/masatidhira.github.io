475a2f14ce5c63efb350739d6c863945e59dc3c0
masatidhira <masatidhira@gmail.com>
2022-10-12 20:08:38 +0700

    Updates

---
+++ precache-manifest.f43b59cebf045d069fdbe75dc813a480.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "039b6b95349d0adc1e21",
+    "url": "/static/css/main.c74c5251.chunk.css"
+  },
+  {
+    "revision": "039b6b95349d0adc1e21",
+    "url": "/static/js/main.039b6b95.chunk.js"
+  },
+  {
+    "revision": "969d1b0e3c9399359801",
+    "url": "/static/js/1.969d1b0e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "77d8e227bc0b23ff606aa09113177e4a",
+    "url": "/index.html"
+  }
+];

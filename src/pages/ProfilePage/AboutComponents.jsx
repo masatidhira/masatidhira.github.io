cee7e877c39667da5cc4d9f0036438c355abfa39
masatidhira <masatidhira@gmail.com>
2023-01-25 01:31:50 +0700

    Update section media query

--- src/pages/ProfilePage/AboutComponents.jsx
+++ src/pages/ProfilePage/AboutComponents.jsx
@@ -1,18 +1,40 @@
 import styled from "styled-components"
 
-export const About = styled.section`
+export const AboutSection = styled.section`
     width: 100%;
     max-width: 768px;
     min-height: 100vh;
     margin: var(--nav-height) auto;
-    padding: var(--nav-height) 0;
-    display: flex;
-    flex-direction: column;
+    padding: var(--nav-height) 3rem;
+    display: grid;
+    grid-template-columns: 1fr;
     position: relative;
+
+    @media (min-width: 720px) {
+        grid-template-columns: 1fr 1fr;
+        padding: var(--nav-height) 4rem;
+    }
 `
 
-export const Section = styled.section`
-    margin: 0 3rem 3rem;
+export const BioSection = styled.section`
+    margin-bottom: 3rem;
+
+    @media (min-width: 720px) {
+        grid-column: span 2;
+    }
+    
+`
+
+export const SkillSection = styled.section`
+    margin-bottom: 3rem;
+`
+
+export const ContactSection = styled.section`
+    margin-bottom: 3rem;
+    
+    @media (min-width: 720px) {
+        text-align: right;
+    }
 `
 
 export const Title = styled.h3`
--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -1,5 +1,5 @@
-import { AuthorName, Container, Heading, HeadingWrapper, Hero, ImageWrapper, Image, ScrollDown } from "./HeroComponents";
-import { About, Section, Title, Text } from "./AboutComponents";
+import { Container, Heading, HeadingWrapper, Hero, ImageWrapper, Image, AuthorName, ScrollDown } from "./HeroComponents";
+import { AboutSection, BioSection, SkillSection, ContactSection, Title, Text } from "./AboutComponents";
 import { Works } from "./WorksComponents"
 import ImgProfile from "../../assets/images/img-profile.jpeg"
 import Data from "./Data"
@@ -21,22 +21,22 @@ const ProfilePage = () => {
                 </ImageWrapper>
                 <ScrollDown />
             </Hero>
-            <About id="about">
-                <Section>
+            <AboutSection id="about">
+                <BioSection>
                     <Title>{Data.introsTitle}</Title>
                     {Data.intros.map((text, i) => <Text key={i}>{text}</Text>)}
-                </Section>
-                <Section>
+                </BioSection>
+                <SkillSection>
                     <Title>{Data.skillsTitle}</Title>
                     {Data.skills.map((text, i) => <Text key={i}>{text}</Text>)}
-                </Section>
-                <Section>
+                </SkillSection>
+                <ContactSection>
                     <Title>{Data.contactsTitle}</Title>
                     {Data.contacts.map((obj, i) => (
                         <Text key={i}><a href={obj.href}>{obj.text}</a></Text>
                     ))}
-                </Section>
-            </About>
+                </ContactSection>
+            </AboutSection>
             <Works id="works">
 
             </Works>
